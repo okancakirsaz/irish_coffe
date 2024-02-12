109d2f7a30b9b56a537c452756361bc7dae7e93a
okancakirsaz <ocakirsaz@gmail.com>
2024-02-12 17:03:24 +0300

    ban system websocket integrated

--- lib/views/authantication/landing_view/viewmodel/landing_viewmodel.dart
+++ lib/views/authantication/landing_view/viewmodel/landing_viewmodel.dart
@@ -1,23 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:irish_coffe/core/init/cache/local_keys_enums.dart';
-import 'package:irish_coffe/core/public_managers/log_out_manager.dart';
 import 'package:irish_coffe/core/public_managers/websocket_manager.dart';
 import '../../../../core/base/viewmodel/base_viewmodel.dart';
 import 'package:mobx/mobx.dart';
-
-import '../../../profile/models/boolean_single_response_model.dart';
-import '../../../profile/models/user_id_send_request_model.dart';
-import '../services/landing_services.dart';
 
 part 'landing_viewmodel.g.dart';
 
 class LandingViewModel = _LandingViewModelBase with _$LandingViewModel;
 
 abstract class _LandingViewModelBase with Store, BaseViewModel {
-  //VARIABLES
-  final LandingServices services = LandingServices();
-  LogOutManager get logOutManager => LogOutManager(viewModelContext);
-
   @override
   void setContext(BuildContext context) => viewModelContext = context;
 
@@ -25,7 +16,6 @@ abstract class _LandingViewModelBase with Store, BaseViewModel {
   Future<int> init() async {
     WebSocketManager.instance.initializeSocketConnection();
     await localeManager.getSharedPreferencesInstance();
-    await isUserPassedBanSystem();
     await localeManager.removeData(LocaleKeysEnums.profileImage.name);
     return 1;
   }
@@ -35,29 +25,6 @@ abstract class _LandingViewModelBase with Store, BaseViewModel {
         localeManager.getNullableStringData(LocaleKeysEnums.token.name);
     if (tokenData != null) {
       return true;
-    } else {
-      return false;
-    }
-  }
-
-  Future<void> isUserPassedBanSystem() async {
-    String? userId =
-        localeManager.getNullableStringData(LocaleKeysEnums.userId.name);
-    if (userId != null) {
-      bool isUserBanned = await _checkIsUserBanned(userId);
-      if (!isUserBanned) {
-        await logOutManager.logOut();
-      }
-    }
-  }
-
-  Future<bool> _checkIsUserBanned(String userId) async {
-    final BooleanSingleResponseModel? response =
-        await services.checkIsUserBanned(UserIdSendRequestModel(uid: userId));
-
-    if (response != null) {
-      //if isSuccess false user got banned
-      return response.isSuccess!;
     } else {
       return false;
     }
--- lib/views/main/viewmodel/main_viewmodel.dart
+++ lib/views/main/viewmodel/main_viewmodel.dart
@@ -1,3 +1,5 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:irish_coffe/core/init/cache/local_keys_enums.dart';
 import 'package:irish_coffe/views/community/view/community_view.dart';
@@ -7,6 +9,13 @@ import 'package:irish_coffe/views/profile/view/profile_view.dart';
 import 'package:mobx/mobx.dart';
 
 import '../../../../core/base/viewmodel/base_viewmodel.dart';
+import '../../../core/consts/color_consts/color_consts.dart';
+import '../../../core/consts/text_consts.dart';
+import '../../../core/public_managers/log_out_manager.dart';
+import '../../../core/public_managers/websocket_manager.dart';
+import '../../authantication/landing_view/services/landing_services.dart';
+import '../../profile/models/boolean_single_response_model.dart';
+import '../../profile/models/user_id_send_request_model.dart';
 
 part 'main_viewmodel.g.dart';
 
@@ -17,7 +26,9 @@ abstract class _MainViewModelBase with Store, BaseViewModel {
   void setContext(BuildContext context) => viewModelContext = context;
   @override
   init() async {
+    listenIsUserBanned();
     await deleteCachedValues();
+    await isUserPassedBanSystem();
   }
 
   Future<void> deleteCachedValues() async {
@@ -33,9 +44,58 @@ abstract class _MainViewModelBase with Store, BaseViewModel {
     const GamesView(),
     const ProfileView(),
   ]);
+  LogOutManager get logOutManager => LogOutManager(viewModelContext);
+
+  //TODO: Move funcs to main services
+  final LandingServices services = LandingServices();
 
   @action
   Widget changePage(int index) {
     return pages[index];
+  }
+
+  listenIsUserBanned() {
+    WebSocketManager.instance.webSocketReceiver("user_banned", (data) async {
+      if (data != null &&
+          data ==
+              localeManager
+                  .getNullableStringData(LocaleKeysEnums.userId.name)) {
+        await showDialog(
+            context: viewModelContext,
+            builder: (context) => AlertDialog(
+                  backgroundColor: ColorConsts.instance.darkGrey,
+                  content: Text(
+                    "Topluluk kurallarımıza aykırı davranışlarınız nedeniyle uygulamadan süresiz olarak uzaklaştırıldınız.",
+                    style: TextConsts.instance.regularWhite16Bold,
+                  ),
+                ));
+        //Close app
+        await logOutManager.logOut();
+        exit(0);
+      }
+    });
+  }
+
+  Future<void> isUserPassedBanSystem() async {
+    String? userId =
+        localeManager.getNullableStringData(LocaleKeysEnums.userId.name);
+    if (userId != null) {
+      bool isUserBanned = await _checkIsUserBanned(userId);
+      if (!isUserBanned) {
+        await logOutManager.logOut();
+      }
+    }
+  }
+
+  Future<bool> _checkIsUserBanned(String userId) async {
+    final BooleanSingleResponseModel? response =
+        await services.checkIsUserBanned(UserIdSendRequestModel(uid: userId));
+
+    if (response != null) {
+      //if isSuccess false user got banned
+      return response.isSuccess!;
+    } else {
+      return false;
+    }
   }
 }
